b1b4663812dd5b353e05a60d5de726ced521c92e
Cassie Pruitt <150861205+cassiepruitt@users.noreply.github.com>
2024-09-24 10:07:25 -0500

    Create animal.h

---
+++ animal.h
@@ -0,0 +1,30 @@
+#ifndef animal_h
+#define animal_h
+#include <string>
+
+class Animal {
+
+public: 
+  Animal() {
+    age = 0;
+    name = " ";
+    type = " ";
+  }
+
+void displayInfo() {
+  std::cout<< "Name: "<< name << "Age: "<< age << "Type: " << type << std::endl;
+  return;
+}
+
+std::string getType() {
+  return type;
+}
+
+private:
+std::string name;
+int age;
+std::string type;
+
+};
+
+#endif
