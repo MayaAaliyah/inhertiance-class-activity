aa11e04f146915f296c30c5aae55a5266c65f7a5
MayaAaliyah <fosterma@uab.edu>
2024-09-24 10:48:10 -0500

    Add files via upload

---
+++ Shelter.h
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+#include "Animal.h"
+#include "Shelter.h"
+
+
+Shelter ::void addAnimal(Animal* animal) {
+	animals.push_back(animals);
+}
+
+
+Shelter::void removeAnimal(std::string name) {
+
+}
+
+
+Shelter::void displayAnimals();
+
+private:
+	std::vector<Animal*> animals;
+};
