2043fd7d64e3afacb58c95df6b7a783f3cb2eb4d
Cassie Pruitt <150861205+cassiepruitt@users.noreply.github.com>
2024-09-26 10:33:00 -0500

    Updated Shelter.h w/comments

--- Shelter.h
+++ Shelter.h
@@ -1,11 +1,14 @@
 #include <vector>
 #include <string>
-#include "Animal.h"
+#include "animal.h"
 #include "Shelter.h"
+
+//Define Shelter as a class
+//Swap the void and Shelter in the function definitions (void Shelter::addAnimal...)
 
 
 Shelter ::void addAnimal(Animal* animal) {
-	animals.push_back(animals);
+	animals.push_back(animals);   //it should be animal inside the parenthese and not animals (it adds the Animal pointer)
 }
 
 
@@ -14,7 +17,7 @@ Shelter::void removeAnimal(std::string name) {
 }
 
 
-Shelter::void displayAnimals();
+Shelter::void displayAnimals(); //Curly brackets needed here instead of semicolon
 
 private:
 	std::vector<Animal*> animals;
